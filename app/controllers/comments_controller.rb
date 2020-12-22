222cf68913b5407d4dfb577a8010b14c8fa4d753
KimFight <mk_idea_lism@icloud.com>
2020-12-22 19:59:59 +0900

    提出のため一回コミット

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,9 @@ class CommentsController < ApplicationController
     redirect_to request.referer
   end
 
+  def destroy
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:comment,:comment_image).merge(user_id: current_user.id, store_id: params[:store_id])
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: 'stores#index'
 
   resources :stores do
-    resources :comments, only: :create 
+    resources :comments, only: [:create, :destroy] 
     resources :favorites, only: [:create, :destroy]
     collection do
       get 'search'
