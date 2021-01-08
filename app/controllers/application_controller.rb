baa8d149745b23d4f319d60e42e06facf1784b0c
KimFight <mk_idea_lism@icloud.com>
2021-01-08 22:40:40 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_search
   before_action :search_store
-  #before_action :basic_auth
+  before_action :basic_auth
 
 
   def set_search
@@ -25,10 +25,10 @@ class ApplicationController < ActionController::Base
     @store_name = Store.select("store_name").distinct
   end
 
-  #def basic_auth
-  #  authenticate_or_request_with_http_basic do |username, password|
-  #    username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-  #  end
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 
 end
